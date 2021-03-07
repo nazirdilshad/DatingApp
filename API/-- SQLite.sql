c898052a4db092a470ab3fecd400630fe6b445d3
Dilshad <dilshadobryan@gmail.com>
2021-03-07 11:43:23 +0530

    api and angular skeleton upto sec3

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO MyProperty (Id, UserName)
+VALUES (1,"dils");
+
+INSERT INTO MyProperty (Id, UserName)
+VALUES (2,"reha");
+
+INSERT INTO MyProperty (Id, UserName)
+VALUES (3,"bbhi");
